daa9bb1ba74c4769656c684eb980112bae39de57
vinayk51 <134974143+vinayk51@users.noreply.github.com>
2023-06-01 12:18:12 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.getName());
+        System.out.println(s.setName(" Vinay"));
+
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name = "Kokkonda";
+    public String getName() {
+        return name;
+    }
+    public String setName(String n) {
+        name = n;
+        return name;
+    }
+}
